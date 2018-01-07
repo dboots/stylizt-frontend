2773d922a6b587d88344ed6bc9128e56b0da7fa9
Don Boots <don@donboots.com>
2018-01-07 15:38:36 -0500

    add home/owner components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
+import { HomeComponent } from './home/home.component';
 import { StylistComponent } from './stylist/stylist.component';
+import { OwnerComponent } from './owner/owner.component';
 
 const routes: Routes = [
-    { path: 'stylist', component: StylistComponent }
+    { path: '', component: HomeComponent },
+    { path: 'stylist', component: StylistComponent },
+    { path: 'owner', component: OwnerComponent },
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,24 +2,27 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
-
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 import { StylistComponent } from './stylist/stylist.component';
-
-import { AppRoutingModule } from './app-routing.module';
+import { OwnerComponent } from './owner/owner.component';
+import { NavComponent } from './nav/nav.component';
+import { HomeComponent } from './home/home.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    StylistComponent
+    StylistComponent,
+    OwnerComponent,
+    NavComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
     NgbModule.forRoot(),
     AppRoutingModule,
-
   ],
   providers: [],
   bootstrap: [AppComponent]

a4b3a24effd1c85ab7da4a5b62aeaefb508ff653
Don Boots <don@donboots.com>
2018-01-07 15:12:57 -0500

    add stylist ng component, vendor links

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 
 import { AppComponent } from './app.component';
 
+import { StylistComponent } from './stylist/stylist.component';
+
+import { AppRoutingModule } from './app-routing.module';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StylistComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgbModule.forRoot(),
+    AppRoutingModule,
+
   ],
   providers: [],
   bootstrap: [AppComponent]

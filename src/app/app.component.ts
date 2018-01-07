b5a20dd47291ef2c54a2599460caf4935736bcd7
Don Boots <don@donboots.com>
2018-01-07 15:53:47 -0500

    add ng dependency

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,7 +7,7 @@ import { Router, NavigationEnd } from '@angular/router';
   styleUrls: ['./app.component.css']
 })
 
-export class AppComponent implements OnInit {
+export class AppComponent {
   constructor(private router: Router) { }
 
   ngOnInit() {

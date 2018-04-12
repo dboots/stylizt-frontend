656d5ba8f55a65a97d61b0986377f8e7acc2d3fc
Don Boots <don@donboots.com>
2018-04-12 11:44:52 -0400

    stylist landing page rework

--- src/app/components/stylist/stylist.component.ts
+++ src/app/components/stylist/stylist.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-stylist',
   templateUrl: './stylist.component.html',
-  styleUrls: ['./stylist.component.css']
+  styleUrls: ['./stylist.component.scss']
 })
 export class StylistComponent implements OnInit {
 

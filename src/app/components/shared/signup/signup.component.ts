134d9daa6f130763a55db25f1943c76ae1673b49
Don Boots <don@donboots.com>
2018-04-15 19:42:47 -0400

    signup tweaks, add GA

--- src/app/components/shared/signup/signup.component.ts
+++ src/app/components/shared/signup/signup.component.ts
@@ -4,7 +4,7 @@ import { SignupService } from '../../../services/signup.service';
 import { Signup } from '../../../models/signup.model';
 
 @Component({
-  selector: 'page-signup',
+  selector: 'app-signup',
   templateUrl: './signup.component.html',
   styleUrls: ['./signup.component.scss']
 })

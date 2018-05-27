35edacad9e724b8f0e4dc9827e5bdc8c7a1c8356
polaris327 <webstar317@yahoo.com>
2018-05-27 16:53:42 +0100

    feat: Create client form using angular formgroup/formcontrols and formvalidators

--- src/app/components/stylist/clients/detail/detail.component.ts
+++ src/app/components/stylist/clients/detail/detail.component.ts
@@ -1,8 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute, ActivationEnd, Params } from '@angular/router';
-import { FormGroup, FormControl, Validators, AbstractControl } from '@angular/forms';
+import { FormBuilder, FormGroup, FormControl, Validators, AbstractControl } from '@angular/forms';
 import { ClientService } from '../../../../services/client.service';
 import { AuthService } from '../../../../services/auth.service';
+import { Client } from '../../../../models/client.model';
 
 @Component({
   selector: 'page-stylistclientsdetail',
@@ -12,30 +13,73 @@ import { AuthService } from '../../../../services/auth.service';
 
 export class StylistClientsDetailPage implements OnInit {    
   detailForm: FormGroup;
-  fcName: FormControl;
+  detailFormErrors: any;
+  clientId: string;
 
   constructor(
     private router: Router,
     private clientService: ClientService,
+    private formBuilder: FormBuilder,
     private activatedRoute: ActivatedRoute,
     private authService: AuthService
   ) {}
 
   ngOnInit() {
-    this.fcName = new FormControl('', [Validators.required]),
-    this.detailForm = new FormGroup({
-      name: this.fcName
-    });
-
     this.activatedRoute.params.subscribe((params: Params) => {
-      let clientId = params['id'];
-      this.clientService.detail(clientId, this.authService.token).subscribe((result) => {
+      this.clientId = params['id'];
+      this.clientService.detail(this.clientId, this.authService.token).subscribe((result) => {
         console.log(result['data']);
       });
     });
+
+    this.initForm();
   }
-  
+
+  initForm() {
+    this.detailFormErrors = {
+      name: {},
+      zip: {}
+    };
+
+    this.detailForm = this.formBuilder.group({
+      name: ['', [Validators.required]],
+      zip: ['', Validators.required]
+    });
+
+    this.formValuesChanged();
+    this.detailForm.valueChanges.subscribe(() => {
+      this.formValuesChanged();
+    });
+  }
+
+  formValuesChanged() {
+    for (const field in this.detailFormErrors) {
+      if (this.detailFormErrors.hasOwnProperty(field)) {
+        this.detailFormErrors[field] = {};
+
+        const control = this.detailForm.get(field);
+
+        if (control && control.dirty && !control.valid) {
+          this.detailFormErrors[field] = control.errors;
+        }
+      }
+    }
+  }
+
   saveClient() {
-    console.log('saving client');
+    const name = this.detailForm.get('name').value;
+    const zipcode = this.detailForm.get('zip').value;
+    const body: Client = {
+      name: this.detailForm.get('name').value,
+      email: '',
+      zip: this.detailForm.get('zip').value,
+      image: '',
+      owner: ''
+    };
+    this.clientService.update(this.clientId, body, this.authService.token).subscribe((result) => {
+      console.log(result);
+    }, (err) => {
+      alert(err.error.messages[0]);
+    });
   }
 }

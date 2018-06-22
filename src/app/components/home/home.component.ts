0fcb2c85008e13faff0f27820c7ace886dd56a11
polaris327 <webstar317@yahoo.com>
2018-06-22 17:08:31 +0100

    fix: refactored the hero section

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -6,9 +6,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomePageComponent implements OnInit {
+  salon: string;
+  salonOptions: string[];
 
   constructor() { }
 
   ngOnInit() {
+    this.salonOptions = ['Bangs', 'Bangs1', 'Bangs2'];
+    this.salon = 'Bangs';
   }
 }

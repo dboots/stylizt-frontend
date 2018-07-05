7b628f3fe45239b971a8bbecb5808b508cd46f97
polaris327 <webstar317@yahoo.com>
2018-07-06 05:28:52 +0800

    feat: Add actual location to "TALENT IN CHICAGO, IL THAT..."

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute, NavigationEnd } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-home',
@@ -9,12 +10,33 @@ import { Router, ActivatedRoute, NavigationEnd } from '@angular/router';
 export class HomePageComponent implements OnInit {
   dropdownOptions: any[];
   currentOption;
+  currentLocation: string;
 
-  constructor() {}
+  constructor(
+    private http: HttpClient
+  ) {}
 
   ngOnInit() {
     this.dropdownOptions = ['DO IT ALL', 'BANGS', 'UPDOS', 'WEAVES'];
     this.currentOption = 'DO IT ALL';
+
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(
+        async (pos) => {
+          const res: any = await this.http.get('http://maps.googleapis.com/maps/api/geocode/json',
+            {
+              params: {
+                latlng: pos.coords.latitude + ',' + pos.coords.longitude
+              }
+            }
+          ).toPromise();
+          this.currentLocation = res.results[0].formatted_address || '';
+        },
+        (err) => {
+          console.log(err);
+        }
+      );
+    }
   }
 
   selectDropdown(option: any) {

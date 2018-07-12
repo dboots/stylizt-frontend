de0a49d58fa558588e246d93ebaa73b6f34d4156
polaris327 <webstar317@yahoo.com>
2018-07-13 04:16:59 +0800

    fix: refactored the contact form to use reactive forms

--- src/app/components/contact/contact.component.ts
+++ src/app/components/contact/contact.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-contact',
@@ -6,11 +7,47 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./contact.component.scss']
 })
 export class ContactPageComponent implements OnInit {
+  contactForm: FormGroup;
+  contactFormErrors: any;
   formSubmitted: boolean;
 
-  constructor() { }
+  constructor(private formBuilder: FormBuilder) { }
 
   ngOnInit() {
+    this.initForm();
+  }
+
+  initForm() {
+    this.contactFormErrors = {
+      name: {},
+      email: {},
+      thoughts: {}
+    };
+
+    this.contactForm = this.formBuilder.group({
+      name: ['', [Validators.required]],
+      email: ['', [Validators.required, Validators.email]],
+      thoughts: ['', Validators.required]
+    });
+
+    this.formValuesChanged();
+    this.contactForm.valueChanges.subscribe(() => {
+      this.formValuesChanged();
+    });
+  }
+
+  formValuesChanged() {
+    for (const field in this.contactFormErrors) {
+      if (this.contactFormErrors.hasOwnProperty(field)) {
+        this.contactFormErrors[field] = {};
+
+        const control = this.contactForm.get(field);
+
+        if (control && control.dirty && !control.valid) {
+          this.contactFormErrors[field] = control.errors;
+        }
+      }
+    }
   }
 
   submit(event) {

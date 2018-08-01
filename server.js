d5c0802a2c98b2f43c49874bf74877184c40356f
Don Boots <don@donboots.com>
2018-08-01 11:42:28 -0400

    debugging deploy

--- server.js
+++ server.js
@@ -11,6 +11,8 @@ app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, '/dist/index.html'));
 });
 
+console.log('attempting to run server on ' + port);
+
 // Start the app by listening on the default Heroku port
 app.listen(port);
 

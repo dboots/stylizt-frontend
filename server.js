8f034ab14fa2633edb2e6c60af9a561fbd5b7e68
polaris327 <webstar317@yahoo.com>
2018-08-15 03:00:15 +0800

    feat: implemented theprerender

--- server.js
+++ server.js
@@ -5,6 +5,11 @@ const port = (process.env.PORT || 8088);
 const path = require('path');
 
 // Serve only the static files form the dist directory
+app.use(
+  require('prerender-node')
+    .set('protocol', 'http')
+    .set('prerenderServiceUrl', 'https://h2c-prerender.herokuapp.com/')
+  );
 app.use(express.static(__dirname + '/dist'));
 
 app.get('*', (req, res) => {

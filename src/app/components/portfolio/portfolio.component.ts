0dba1e55f797d9946767322be9ef96c3c887f993
Don Boots <don@donboots.com>
2018-09-05 13:03:38 -0400

    add social media to portfolio

--- src/app/components/portfolio/portfolio.component.ts
+++ src/app/components/portfolio/portfolio.component.ts
@@ -16,6 +16,10 @@ export class StylistPortfolioPageComponent implements OnInit {
   params;
   mapUrl: SafeResourceUrl;
 
+  instagram: string = null;
+  twitter: string = null;
+  facebook: string = null;
+
   constructor(
     private portfolioService: PortfolioService,
     private route: ActivatedRoute,
@@ -26,18 +30,45 @@ export class StylistPortfolioPageComponent implements OnInit {
     this.route.params.subscribe((params) => { this.params = params });
 
     this.portfolioService.read({owner: this.params.id}).subscribe((data: any) => {
-      this.portfolio = data.portfolio;
-      this.stylist = data.stylist;
+      let portfolio = data.portfolio;
+      let stylist = data.stylist;
 
-      this.title.setTitle(this.stylist.name + ' Portfolio');
+      this.title.setTitle(stylist.name + ' Portfolio');
       this.meta.updateTag(
-        { name: 'description', content: this.stylist.name + ' is a stylist from ' + this.stylist.zip }
+        { name: 'description', content: stylist.name + ' is a stylist from ' + stylist.zip }
       );
       
-      var location = this.stylist.zip.replace('#', '%23');
+      var location = stylist.zip.replace('#', '%23');
       let url = 'https://maps.google.com/maps?width=100%&height=600&hl=en&q=' + encodeURI(location) + '&ie=UTF8&t=&z=14&iwloc=B&output=embed';
       this.mapUrl = this.sanitizer.bypassSecurityTrustResourceUrl(url);
+
+      this.twitter = (stylist.twitter) ? this.getUrl(stylist.twitter, 'http://www.twitter.com/') : null;
+      this.facebook = (stylist.facebook) ? this.getUrl(stylist.facebook, 'http://www.facebook.com/') : null;
+      this.instagram = (stylist.instagram) ? this.getUrl(stylist.instagram, 'http://www.instagram.com/') : null;
+
+      this.portfolio = portfolio;
+      this.stylist = stylist;
     })
+  }
+
+  // Process social media url/handles
+  // handle could be valid url, username, @username
+  // return url should be close to http://www.[social].com/[handle]
+  getUrl(handle: string, url: string): string {
+    let hasHttp = (handle.indexOf('http') > -1);
+    let hasHttps = (handle.indexOf('https') > -1);
+    let hasWww = (handle.indexOf('www') > -1);
+    let isUrl = (hasHttp || hasHttps || hasWww);
+
+    if ((!hasHttp && !hasHttps) && hasWww) {
+      handle = 'https://' + handle;
+    }
+
+    if (isUrl) {
+      return handle;
+    }
+
+    return url + handle.replace('@', '');
   }
 
   ngOnInit() {

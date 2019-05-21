987c99a6ea49ebc9d39c935e27e37cd78de1d042
Don Boots <don@donboots.com>
2019-05-21 18:46:12 -0400

    allow stripe to be bypassed

--- src/app/components/shared/signup/signup.component.ts
+++ src/app/components/shared/signup/signup.component.ts
@@ -3,7 +3,7 @@ import { Observable } from 'rxjs';
 import { Component, Input, NgZone } from '@angular/core';
 import { AuthService, UserService } from '../../../services';
 import { User } from '../../../models';
-import { Router } from '@angular/router';
+import { Router, ActivatedRoute, RouterStateSnapshot, Params } from '@angular/router';
 import { AbstractControl, FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
@@ -19,122 +19,136 @@ export class SignupComponent {
   message = '';
   showSignup = true;
   success = false;
-
+  
   signupForm: FormGroup;
   signupFormErrors: any;
 
+  urlParams: Params;
+  
   constructor(
     private formBuilder: FormBuilder,
     private userService: UserService,
     private authService: AuthService,
     private router: Router,
+    private route: ActivatedRoute,
     private zone: NgZone
-  ) {
-    this.initForm();
-  }
-
-  initForm() {
-    this.signupFormErrors = {
-      name: {},
-      email: {},
-      password: {},
-      passwordRepeat: {}
-    };
-
-    this.signupForm = this.formBuilder.group({
-      name: ['', Validators.required],
-      email: ['', [Validators.required, Validators.email]],
-      password: ['', Validators.required],
-      passwordRepeat: ['', [Validators.required, confirmPassword]],
-    });
-
-    this.formValuesChanged();
-    this.signupForm.valueChanges.subscribe(() => {
+    ) {
+      this.initForm();
+      this.route.queryParams.subscribe(params => {
+        this.urlParams = params;
+      });
+    }
+    
+    initForm() {
+      this.signupFormErrors = {
+        name: {},
+        email: {},
+        password: {},
+        passwordRepeat: {}
+      };
+      
+      this.signupForm = this.formBuilder.group({
+        name: ['', Validators.required],
+        email: ['', [Validators.required, Validators.email]],
+        password: ['', Validators.required],
+        passwordRepeat: ['', [Validators.required, confirmPassword]],
+      });
+      
       this.formValuesChanged();
-    });
-  }
-
-  formValuesChanged() {
-    for (const field in this.signupFormErrors) {
-      if (this.signupFormErrors.hasOwnProperty(field)) {
-        this.signupFormErrors[field] = {};
-
-        const control = this.signupForm.get(field);
-
-        if (control && control.dirty && !control.valid) {
-          this.signupFormErrors[field] = control.errors;
+      this.signupForm.valueChanges.subscribe(() => {
+        this.formValuesChanged();
+      });
+    }
+    
+    formValuesChanged() {
+      for (const field in this.signupFormErrors) {
+        if (this.signupFormErrors.hasOwnProperty(field)) {
+          this.signupFormErrors[field] = {};
+          
+          const control = this.signupForm.get(field);
+          
+          if (control && control.dirty && !control.valid) {
+            this.signupFormErrors[field] = control.errors;
+          }
         }
       }
     }
-  }
+    
+    signup() {
+      var model = this.model;
+      model.type = this.type;
+      model.name = this.signupForm.get('name').value;
+      model.email = this.signupForm.get('email').value;
+      model.password = this.signupForm.get('password').value;
 
-  signup() {
-    var model = this.model;
-    model.type = this.type;
-    model.name = this.signupForm.get('name').value;
-    model.email = this.signupForm.get('email').value;
-    model.password = this.signupForm.get('password').value;
-
-    stripe.open({
-      email: model.email,
-      name: 'Hair to Chair',
-      description: 'Hair to Chair - Basic',
-      amount: 499,
-      token: (source) => {
+      if (typeof this.urlParams.bypass !== 'undefined') {
         this.userService.signup(model).subscribe((data: User) => {
           let token = data['token'];
           localStorage.setItem('token', token);
-
-          this.userService.createSubscription(token, source.id).subscribe(_ => {
-            this.zone.run(() => {
-              this.router.navigate(['stylist/profile']);
+          this.router.navigate(['stylist/profile']);
+        });
+      } else {
+        stripe.open({
+          email: model.email,
+          name: 'Hair to Chair',
+          description: 'Hair to Chair - Basic',
+          amount: 499,
+          token: (source) => {
+            this.userService.signup(model).subscribe((data: User) => {
+              let token = data['token'];
+              localStorage.setItem('token', token);
+              
+              this.userService.createSubscription(token, source.id).subscribe(_ => {
+                this.zone.run(() => {
+                  this.router.navigate(['stylist/profile']);
+                });
+              });
             });
-          });
+          }
+        }, (error) => {
+          this.message = error.error;
+          this.authService.logout();
+          return Observable.throw(error);
         });
       }
-    }, (error) => {
-      this.message = error.error;
-      this.authService.logout();
-      return Observable.throw(error);
-    });
-  }
-
-  login() {
-    let model = this.model;
-    model.email = this.signupForm.get('email').value;
-    model.password = this.signupForm.get('password').value;
-
-    this.userService.login(model).subscribe(
-      (data) => {
-        this.message = 'Thanks for logging in!';
-        return true;
-      }, (error) => {
-        this.message = 'Invalid email address';
-        return Observable.throw(error);
+    }
+    
+    login() {
+      let model = this.model;
+      model.email = this.signupForm.get('email').value;
+      model.password = this.signupForm.get('password').value;
+      
+      this.userService.login(model).subscribe(
+        (data) => {
+          this.message = 'Thanks for logging in!';
+          return true;
+        }, (error) => {
+          this.message = 'Invalid email address';
+          return Observable.throw(error);
+        }
+        );
       }
-    );
-  }
-}
-
-function confirmPassword(control: AbstractControl) {
-  if (!control.parent || !control) {
-    return;
-  }
-
-  const password = control.parent.get('password');
-  const passwordRepeat = control.parent.get('passwordRepeat');
-
-  if (!password || !passwordRepeat) {
-    return;
-  }
-
-  if (passwordRepeat.value === '') {
-    return;
-  }
-
-  if (password.value !== passwordRepeat.value) {
-    return {
-      passwordsNotMatch: true
-    };
-  }
-}
+    }
+    
+    function confirmPassword(control: AbstractControl) {
+      if (!control.parent || !control) {
+        return;
+      }
+      
+      const password = control.parent.get('password');
+      const passwordRepeat = control.parent.get('passwordRepeat');
+      
+      if (!password || !passwordRepeat) {
+        return;
+      }
+      
+      if (passwordRepeat.value === '') {
+        return;
+      }
+      
+      if (password.value !== passwordRepeat.value) {
+        return {
+          passwordsNotMatch: true
+        };
+      }
+    }

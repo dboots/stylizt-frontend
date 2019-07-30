968f41f1580008a6308125379d71a16319399a67
Don Boots <don@donboots.com>
2019-07-30 19:46:23 -0400

    refine upload component

--- src/app/components/shared/cloudinary-uploader/cloudinary-uploader.component.ts
+++ src/app/components/shared/cloudinary-uploader/cloudinary-uploader.component.ts
@@ -10,6 +10,7 @@ export class CloudinaryUploaderComponent implements OnInit {
   uploader: FileUploader;
   @Input() folderName: string;
   @Input() buttonName: string;
+  @Input() allowMultiple: boolean = false;
   @Output() completeItem: EventEmitter<any> = new EventEmitter();
   @Output() afterAddingFile: EventEmitter<any> = new EventEmitter();
   @Output() whenAddingFileFailed: EventEmitter<any> = new EventEmitter();
@@ -17,8 +18,9 @@ export class CloudinaryUploaderComponent implements OnInit {
   @Output() uploadComplete: EventEmitter<any> = new EventEmitter();
   @Output() startUpload: EventEmitter<any> = new EventEmitter();
   showLoadingScreen: boolean;
-  loadingProgress: any;
+  loadingProgress: number = 0;
   fileCount: number = 0;
+  currentFile: number = 0;
 
   constructor() { }
 
@@ -53,12 +55,17 @@ export class CloudinaryUploaderComponent implements OnInit {
       // TODO: delete old file
       response = JSON.parse(response);
       this.completeItem.emit(response);
-      this.loadingProgress = 0;
-      this.fileCount--;
+      this.currentFile++;
 
-      if (this.fileCount === 0) {
+      if (this.fileCount === this.currentFile) {
         this.showLoadingScreen = false;
         this.uploadComplete.emit();
+        this.loadingProgress = 0;
+
+        if (!this.allowMultiple) {
+          this.fileCount = 0;
+          this.currentFile = 0;
+        }
       }
     };
 

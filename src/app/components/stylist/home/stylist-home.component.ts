8a1ffb4271455766ace482e4869a4170876f5ee3
Don Boots <don@donboots.com>
2019-08-19 14:40:32 -0400

    stylist schedule ui updates

--- src/app/components/stylist/home/stylist-home.component.ts
+++ src/app/components/stylist/home/stylist-home.component.ts
@@ -10,17 +10,37 @@ import { User, Schedule } from '../../../models';
 export class StylistHomePageComponent implements OnInit {
   user: User;
   schedule: Schedule[] = [];
+  scheduleDay: Date = new Date();
 
   constructor(private authService: AuthService, private scheduleService: ScheduleService) {
-    let today = new Date();
-    today.setHours(9);
-    today.setMinutes(0);
+    this.scheduleDay.setHours(9);
+    this.scheduleDay.setMinutes(0);
 
     this.user = this.authService.decode();
-    this.scheduleService.read(this.user._id, today.getTime()).subscribe((result) => {
-      console.log(result);
+    this.loadSchedule();
+  }
+
+  loadSchedule() {
+    console.log('getting schedule for', this.scheduleDay.getMonth(), this.scheduleDay.getDay(), this.scheduleDay.getFullYear());
+    this.scheduleService.read(this.user._id, this.scheduleDay.getTime()).subscribe((result) => {
       this.schedule = result['data'];
     });
+  }
+
+  nextScheduleDay() {
+    let date = new Date(this.scheduleDay);
+    date.setHours(date.getHours() + 24);
+
+    this.scheduleDay = date;
+    this.loadSchedule();
+  }
+
+  prevScheduleDay() {
+    let date = new Date(this.scheduleDay);
+    date.setHours(date.getHours() - 24);
+
+    this.scheduleDay = date;
+    this.loadSchedule();
   }
 
   ngOnInit() { }

2af96bb3c5bf23c42b8deaf6eb201b02758162cf
Don Boots <don@donboots.com>
2020-03-21 15:39:09 -0400

    steps updates

--- src/app/components/landing/onboarding/steps/five/five.component.ts
+++ src/app/components/landing/onboarding/steps/five/five.component.ts
@@ -40,6 +40,7 @@ export class StepFiveComponent implements OnInit {
     this.clientService.create(body).subscribe((result) => {
       this.formGroup.reset();
       this.clients.push(result['data']);
+      this.uploadImage = null;
     });
   }
 

09bb658bd6aa8eafb293d01e8f3a61045f91c953
Don Boots <don@donboots.com>
2020-03-23 19:42:57 -0400

    update blog styles

--- src/app/components/blog/post/post.component.ts
+++ src/app/components/blog/post/post.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { PostService } from '../../../services/post.service';
 import { DomSanitizer } from '@angular/platform-browser';
@@ -6,11 +6,12 @@ import { DomSanitizer } from '@angular/platform-browser';
 @Component({
   selector: 'app-post',
   templateUrl: './post.component.html',
-  styleUrls: ['./post.component.scss']
+  styleUrls: ['./post.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 export class BlogPostComponent implements OnInit {
   slug: string;
-  post: any = {};
+  post: any;
   relatedPosts: any = [];
 
   constructor(

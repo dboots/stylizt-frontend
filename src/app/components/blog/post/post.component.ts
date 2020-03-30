dd00f5cd5e7912465262dddd48993e7a80d44a4d
Don Boots <don@donboots.com>
2020-03-30 18:18:52 -0400

    update post meta tags

--- src/app/components/blog/post/post.component.ts
+++ src/app/components/blog/post/post.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit, ViewEncapsulation, Inject } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { PostService } from '../../../services/post.service';
-import { DomSanitizer } from '@angular/platform-browser';
+import { DomSanitizer, Meta, Title } from '@angular/platform-browser';
 import { DOCUMENT } from '@angular/common';
 
 @Component({
@@ -19,6 +19,8 @@ export class BlogPostComponent implements OnInit {
     private route: ActivatedRoute,
     private postService: PostService,
     private sanitizer: DomSanitizer,
+    private meta: Meta,
+    private title: Title,
     @Inject(DOCUMENT) private dom
   ) { }
 
@@ -26,13 +28,42 @@ export class BlogPostComponent implements OnInit {
     this.route.paramMap.subscribe((params) => {
       this.slug = params.get('slug');
       this.postService.read(this.slug).subscribe((result) => {
+        console.log(result.data);
         this.post = result.data;
         this.sanitizer.bypassSecurityTrustHtml(this.post.html);
 
+        // TODO: Move to a SEO Service
         let link: HTMLLinkElement = this.dom.createElement('link');
         link.setAttribute('rel', 'canonical');
         this.dom.head.appendChild(link);
         link.setAttribute('href', this.dom.URL);
+
+        this.title.setTitle(this.post.title);
+
+        this.meta.updateTag({
+          name: 'title',
+          content: this.post.meta_title
+        });
+
+        this.meta.updateTag({
+          name: 'description',
+          content: this.post.meta_description
+        });
+
+        this.meta.updateTag({
+          name: 'author',
+          content: 'Hair to Chair'
+        });
+
+        this.meta.updateTag({
+          name: 'publisher',
+          content: 'Hair to Chair'
+        });
+
+        this.meta.updateTag({
+          name: 'robots',
+          content: 'index, follow (same for all)'
+        });
       });
     });
 

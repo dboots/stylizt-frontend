416de624df6ed8d67da0b028fb0ecc565165cb1f
Don Boots <don@donboots.com>
2020-04-26 00:39:21 -0400

    fix tag error

--- src/app/components/blog/tag/tag.component.ts
+++ src/app/components/blog/tag/tag.component.ts
@@ -29,10 +29,11 @@ export class BlogTagComponent implements OnInit {
       let posts: any[];
 
       this.postService.browse({ tag: slug }).subscribe((result: any) => {
+        console.log(result);
+        tag = result.data[0].tags.filter((item: any) => item.slug === slug)[0];
         let metaTitle: string = tag.meta_title || tag.name;
         let metaDescription: string = tag.meta_description || tag.description || `Blog posts for ${tag.name}`;
         posts = result.data;
-        tag = posts[0].tags.filter((item: any) => item.slug === slug)[0];
 
         this.seoService.createCanonicalUrl();
         this.seoService.updateMetaTags(metaTitle, null, metaDescription);

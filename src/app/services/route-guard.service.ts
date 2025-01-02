50a63bfd660fd7039f3c0a1be2b4f504d9f10a86
dboots <don.boots.ctr@govini.com>
2025-01-02 01:52:03 -0500

    ng16 upgrade

--- src/app/services/route-guard.service.ts
+++ src/app/services/route-guard.service.ts
@@ -1,9 +1,9 @@
 import { Injectable } from '@angular/core';
-import { Router, CanActivate, ActivatedRoute } from '@angular/router';
+import { Router, ActivatedRoute } from '@angular/router';
 import { AuthService } from './auth.service';
 
 @Injectable()
-export class AuthGuardService implements CanActivate {
+export class AuthGuardService  {
   constructor(
     public auth: AuthService,
     public router: Router,
